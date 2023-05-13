68776e122031c1bad0ec2eb52ceae1969053e9da
Rene <renatatwins18@gmail.com>
2023-05-13 15:17:42 +0300

    auth errors

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,14 +5,14 @@ const UnauthorizedError = require('../errors/UnauthorizedError');
 const auth = async (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new UnauthorizedError('Необходима авторизация');
+    next(new UnauthorizedError('Необходима авторизация'));
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = await jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    throw new UnauthorizedError('Необходима авторизация');
+    next(new UnauthorizedError('Необходима авторизация'));
   }
   req.user = payload;
   next();
